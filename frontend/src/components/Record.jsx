6044828ed610474f3cd98d728553a9830d271dd1
Lindsey <codelikeagirl91@gmail.com>
2024-10-12 01:25:04 -0500

    changed form

--- frontend/src/components/Record.jsx
+++ frontend/src/components/Record.jsx
@@ -88,7 +88,7 @@ export default function Record() {
         onSubmit={onSubmit}
         className="border rounded-lg overflow-hidden p-4"
       >
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-slate-900/10 pb-12 md:grid-cols-2 card bordered">
           <div>
             <h2 className="text-base font-semibold leading-7 text-slate-900">
               Employee Info
@@ -100,12 +100,12 @@ export default function Record() {
           </div>
 
           <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 ">
-            <div className="sm:col-span-4">
+            <div className="sm:col-span-4 form-control">
               <label
                 htmlFor="name"
-                className="block text-sm font-medium leading-6 text-slate-900 input input-bordered flex items-center gap-2"
+                className="block text-sm font-medium leading-6 text-slate-900 flex items-center gap-2 label cursor-pointer"
               >
-                Name
+              <span className="label-text">Name</span>
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
@@ -113,7 +113,7 @@ export default function Record() {
                     type="text"
                     name="name"
                     id="name"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6 grow"
+                    className="outline w-full p-4 bg-transparent text-gray-600 grow"
                     placeholder="First & Last Name"
                     value={form.name}
                     onChange={(e) => updateForm({ name: e.target.value })}
@@ -121,12 +121,12 @@ export default function Record() {
                 </div>
               </div>
             </div>
-            <div className="sm:col-span-4">
+            <div className="sm:col-span-4 form-control">
               <label
                 htmlFor="position"
-                className="block text-sm font-medium leading-6 text-slate-900 input input-bordered flex items-center gap-2"
+                className="block text-sm font-medium leading-6 text-slate-900 flex items-center gap-2 label cursor-pointer"
               >
-                Position
+                <span className="label-text">Position</span>
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-slate-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
@@ -134,7 +134,7 @@ export default function Record() {
                     type="text"
                     name="position"
                     id="position"
-                    className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-slate-900 placeholder:text-slate-400 focus:ring-0 sm:text-sm sm:leading-6 grow"
+                    className="outline w-full p-4 bg-transparent text-gray-600 grow"
                     placeholder="Project Manager"
                     value={form.position}
                     onChange={(e) => updateForm({ position: e.target.value })}
@@ -143,8 +143,8 @@ export default function Record() {
               </div>
             </div>
             <div>
-              <fieldset className="mt-4">
-                <legend className="sr-only">Position Options</legend>
+              <fieldset className="mt-4 form-control">
+                <legend className="label-text">Level</legend>
                 <div className="space-y-4 sm:flex sm:items-center sm:space-x-10 sm:space-y-0">
                   <div className="flex items-center">
                     <input
@@ -152,7 +152,7 @@ export default function Record() {
                       name="positionOptions"
                       type="radio"
                       value="Intern"
-                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      className="h-4 w-4 text-slate-600 focus:ring-slate-600 cursor-pointer radio radio-primary"
                       checked={form.level === "Intern"}
                       onChange={(e) => updateForm({ level: e.target.value })}
                     />
@@ -167,7 +167,7 @@ export default function Record() {
                       name="positionOptions"
                       type="radio"
                       value="Junior"
-                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer radio radio-primary"
                       checked={form.level === "Junior"}
                       onChange={(e) => updateForm({ level: e.target.value })}
                     />
@@ -182,7 +182,7 @@ export default function Record() {
                       name="positionOptions"
                       type="radio"
                       value="Senior"
-                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer"
+                      className="h-4 w-4 border-slate-300 text-slate-600 focus:ring-slate-600 cursor-pointer radio radio-primary"
                       checked={form.level === "Senior"}
                       onChange={(e) => updateForm({ level: e.target.value })}
                     />
@@ -201,7 +201,7 @@ export default function Record() {
         <input
           type="submit"
           value="Save Employee Record"
-          className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4"
+          className="inline-flex items-center justify-center whitespace-nowrap text-md font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 hover:text-accent-foreground h-9 rounded-md px-3 cursor-pointer mt-4 btn btn-outline btn-secondary"
         />
       </form>
     </>
